c9e7867026e2634f0e8c7bb60611396207826bbe
auriml <aureliabustos@gmail.com>
2018-04-12 15:39:43 +0200

    adding methods to describe/study SJ dataset

--- util.py
+++ util.py
@@ -12,30 +12,47 @@ import os
 
 import requests, zipfile, io
 import pandas as pd
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Util methods')
 parser.add_argument('-s', metavar='MR_ID_XNAT', type=str, nargs=1,
                     help='display DICOM info for all images of a study identified with MR ID XNAT')
-parser.add_argument('-p', metavar='MR_ID_XNAT', type=str, nargs=1,
+parser.add_argument('-ps', metavar='MR_ID_XNAT', type=str, nargs=1,
                     help='display DICOM info for all images of all studies available for a patient that has a study identified with MR ID XNAT')
+parser.add_argument('-p', metavar='PatientID', type=str, nargs=1,
+                    help='display DICOM info for all images of all studies available for a patient identified with patientID')
+
 parser.add_argument('-m', metavar='DICOM field modality',
                     help='summarize all values in dataset for the DICOM field modality')
 parser.add_argument('-f',  metavar='filename', type=str, nargs='?', default= True,
                     help='save in filename all info for all studies. Default filename: /all_info_studies.csv  ')
 parser.add_argument('-d',  metavar='filename', type=str, nargs='?', default= True,
                     help='describe all info for all studies. Default filename: /all_info_studies.csv  ')
+parser.add_argument('-dc',  metavar='categorical_field', type=str, nargs=1,
+                    help='summarize DICOM field categories for all studies. Default filename: /all_info_studies.csv. Possible categorical fields are '
+                         'Modality;SeriesDescription;ProtocolName;ViewPosition;PhotometricInterpretation')
+parser.add_argument('-dn',  metavar='numerical_field', type=str, nargs=1,
+                    help='summarize DICOM numerical field  for all studies. Default filename: /all_info_studies.csv. Possible numerical fields are '
+                         'Rows;Columns;PixelAspectRatio;SpatialResolution;RelativeXRayExposure')
+
+parser.add_argument('-split',action='store_true',
+                    help='split in side and front views based on DICOM info. Default source filename: /all_info_studies.csv.')
 
 #parser.add_argument('u', metavar='username', type=str, nargs=1,
                     #help='XNAT account username')
 #parser.add_argument('p', metavar='password', type=str, nargs=1,
                     #help='XNAT account password')
-
+all_info_studies_file = '/all_info_studies.csv'
 args = parser.parse_args()
 ID_XNAT = args.s[0] if args.s  else  None
-patient_ID_XNAT = args.p[0] if args.p  else  None
+patient_ID_XNAT = args.ps[0] if args.ps  else  None
+patient_ID = args.p[0] if args.p  else  None
 modality = args.m if args.m  else  None
-filename =  '/all_info_studies.csv' if args.f is None else None
-filename_to_describe =  '/all_info_studies.csv' if args.d is None else None
+filename =  all_info_studies_file if args.f is None else None
+filename_to_describe =  all_info_studies_file if args.d is None else None
+categorical_field = args.dc if args.dc  else  None
+numerical_field = args.dn if args.dn  else  None
+split_images_side_front = args.split if args.split  else  None
 
 
 #j_username = args.u[0] if args.u  else  ''
@@ -135,7 +152,7 @@ def getAllInfo(ID_XNAT, dataset_asoc_DF = None, report_DF = None):
         images_info['Report'] = study_info['report']
         images_info['StudyDate'] = value.StudyDate if 'StudyDate' in value else None
         images_info['Modality'] = pattern.sub('', value.Modality) if 'Modality' in value else None
-        images_info['SeriesDescription'] = pattern.sub('', value.SeriesDescription) if 'SeriesDescription' in value else None#//TODO: summarize values, e.g. APhorizontal
+        images_info['SeriesDescription'] = pattern.sub('', value.SeriesDescription) if 'SeriesDescription' in value else None
         images_info['ProtocolName'] = pattern.sub('', value.ProtocolName) if 'ProtocolName' in value else None #//TODO: summarize values, e.g. Trax ??
         images_info['ViewPosition'] = pattern.sub('', value.ViewPosition) if 'ViewPosition' in value else None #//TODO: summarize values
         images_info['Rows'] = value.Rows if 'Rows' in value else None
@@ -148,7 +165,7 @@ def getAllInfo(ID_XNAT, dataset_asoc_DF = None, report_DF = None):
 
     return images
 
-def saveAllStudyInfoFullDataset(save_file = '/all_info_studies.csv', dataset_asoc_file = '/dataset_asoc_abril18.csv'):
+def saveAllStudyInfoFullDataset(save_file = all_info_studies_file, dataset_asoc_file = '/dataset_asoc_abril18.csv'):
     dataset_asoc = '/dataset_asoc_abril18.csv' if not dataset_asoc_file else dataset_asoc_file
     path = root + '/Rx-thorax-automatic-captioning' + dataset_asoc
     dataset_asoc_DF = pd.read_csv(path, sep = ';', )
@@ -208,29 +225,176 @@ def summarizeAllStudiesDicomModality():
     print(samples)
     return fieldValues, samples
 
-def summarizeAllStudies(file = '/all_info_studies.csv'):
+def splitImagesFrontalSide(file = all_info_studies_file):
+    all_studies_DF = pd.read_csv(root + '/Rx-thorax-automatic-captioning' + file, sep = ';' , header = 0)
+
+    #Side view images where StudyDescription is in lat array (a manual selection from values of StudyDescription,
+    # please run summarizeAllStudiesByCategory when new images are added to dataset to identify new values)
+    lat = ["Lateral","Lateralizq", "LatVertical", "LatHorizontal", "Decblatizq"]
+    side_images = all_studies_DF[all_studies_DF['SeriesDescription'].isin(lat)]
+
+    #Front view images where StudyDescription is in front array (a manual selection from values of StudyDescription,
+    # please run summarizeAllStudiesByCategory when new images are added to dataset to identify new values)
+    front = ["Trax","Tórax","PA", "PAhoriz","APhorizontal","PAvertical", "pulmon", "AP","torax","APhoriz", "APvertical",
+    "Lordtica", "APHorizontal", "PAHorizontal","Pediatra3aos", "Pediatría3años","APVertical", "Pedit3aos",  "Pediát3años", "W033TraxPA"]
+    front_images = all_studies_DF[all_studies_DF['SeriesDescription'].isin(front)]
+
+    side_images.to_csv('side_images.csv')
+    front_images.to_csv('front_images.csv')
+
+    return {'side' : side_images, 'front': front_images }
+
+def summarizeAllStudiesByCategory (file = all_info_studies_file, byCategory = None):
+    #Return dictionary where each key is one class and each value is a tuple with number of ocurrences and one example
+    dict = {}
+    all_studies_DF = pd.read_csv(root + '/Rx-thorax-automatic-captioning' + file, sep = ';' , header = 0)
+    n = all_studies_DF.groupby([byCategory]).count()
+
+    return n
+def summarizeAllStudies(file = all_info_studies_file):
     summary = {}
     all_studies_DF = pd.read_csv(root + '/Rx-thorax-automatic-captioning' + file, sep = ';' , header = 0)
 
+    #Number of different images
+    n = all_studies_DF['ImagePath'].count()
+    print("Number of different images: " + str(n))
     #Number of different patients
-    n = all_studies_DF.groupby(['PatientID']).count()
-    print("Number of different patients :" + str(n))
-    #Number of studies per patient (mean, min, max)
-    #Age (mean, min, max), (distribution by year)
-    #Number of images per study (mean, min, max)
-    #Study date (distribution by year)
-    #Modality (distribution by modality)
-    #Series Description (distribution)
-    #
+    np = all_studies_DF.groupby(['PatientID']).count()
+    print("Number of different patients: " + str(len(np)))
+    print ("Distribution of # of images by patient")
+    print ( np['StudyID'].describe())
+    #ax = serie.hist()
+    #fig = ax.get_figure()
+    #fig.savefig('PatientNumberDistribution.pdf')
+    #import matplotlib.pyplot as plt
+    #s.hist()
+    #plt.savefig('path/to/figure.pdf')  # saves the current figure
 
+    #Number of studies per patient (mean, min, max)
+    ns = all_studies_DF.groupby(['StudyID']).count()
+    print("Number of different studies: " + str(len(ns)))
+    #Number of images per study (mean, min, max)
+    print("Distribution of # of images per study (mean, min, max): ")
+    ns = all_studies_DF.groupby(['StudyID'])['ImagePath'].nunique()
+    print(ns.describe())
+    ns = all_studies_DF.groupby(['PatientID'])['StudyID'].nunique()
+    print("Distribution of # of studies per patient (mean, min, max): ")
+    print(ns.describe())
+    patients_with_multiple_studies= ns[ns > 1]
+    patients_with_multiple_studies.hist()
+    plt.title("Histogram of Patients with Multiple Studies (n = " + str(patients_with_multiple_studies.count()) + ")" )
+    plt.xlabel("Number of Studies")
+    plt.ylabel("Number of Patients")
+    plt.savefig('StudiesPerPatientHistogram.pdf')
+    plt.gcf().clear()
+    patients_with_multiple_studies.to_csv('Patients_with_multiple_studies.csv')
+
+
+    #Birth year (mean, min, max), (distribution by year)
+    print ('Birth Year Distribution: ')
+    years = all_studies_DF['PatientBirth']
+    years = pd.to_numeric(years, errors='coerce')
+    print (years.describe())
+    years.hist()
+    plt.title("Birth Year Histogram (n = " + str(n) + ")" )
+    plt.xlabel("Patient's Birth Year")
+    plt.ylabel("Images")
+    plt.savefig('BirthYearHistogram.pdf')  # saves the current figure
+    plt.gcf().clear()
+
+    #Age year  (mean, min, max), (distribution by year)
+    print ('Age Distribution: ')
+    study_dates = all_studies_DF['StudyDate'].apply(lambda x: x[:4])
+    ages = pd.to_numeric(study_dates, errors='coerce' ) -  years
+    print (ages.describe())
+    ages.hist()
+    plt.title("Age Histogram (n = " + str(n) + ")" )
+    plt.xlabel("Patient's Age")
+    plt.ylabel("Images")
+    plt.savefig('AgeHistogram.pdf')
+    plt.gcf().clear()
+
+
+
+    #Study date (distribution by year)
+    print('Study Date Distribution')
+    study_dates = pd.to_numeric(all_studies_DF['StudyDate'], errors='coerce' ).dropna()
+    study_dates = study_dates.apply(lambda x: str(x)[:4] + '-' + str(x)[4:6] + '-' + str(x)[6:8]).astype('datetime64[ns]')
+    years = pd.DatetimeIndex(study_dates).year.values
+    pd.Series(years).hist()
+    plt.title("Study Year Histogram (n = " + str(n) + ")" )
+    plt.xlabel("Year of Study")
+    plt.ylabel("Images")
+    plt.savefig('StudyYearHistogram.pdf')
+    plt.gcf().clear()
+
+
+    months = pd.DatetimeIndex(study_dates).month.values
+    month_names = ['JAN', 'FEB', 'MAR', 'APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+    freq = pd.Series(months).value_counts().sort_index()
+    freq.plot(kind='bar')
+    plt.title("Study Month Histogram (n = " + str(n) + ")" )
+    plt.xlabel("Month of Study")
+    plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11], month_names)
+    plt.ylabel("Images")
+    plt.savefig('StudyMonthHistogram.pdf')
+    plt.gcf().clear()
+
+
+
+    #Modality (distribution by modality) //TODO: remove CT aprox 800
+    print('Study Modality Distribution')
+    modalities = all_studies_DF['Modality'].value_counts()
+    print(modalities)
+    #Series Description (distribution) //TODO:
+# remove residuals:
+# Costillasobl812       4
+# LatVertical           4
+# Costillasobl17        3
+# PAHorizontal          3
+# APHorizontal          3
+# CostillasAP812        3
+# CostillasAP17         2
+# LatHorizontal         2
+# Decblatizq            1
+# Pediatra3aos          1
+# APVertical            1
+# W033TraxPA            1
+# Pediát3años           1
+# Pediatría3años        1
+# None                  1
+# Pedit3aos             1
+    #Modality (distribution by modality)
+    print('Study Description Distribution')
+    modalities = all_studies_DF['SeriesDescription'].value_counts()
+    print(modalities)
+
+    #Images without reports
+    no_reports = all_studies_DF[all_studies_DF['Report'] == 'None']
+    print ("Studies with no reports:")
+    nr = '\n'.join(no_reports.groupby('StudyID').groups.keys())
+    f = open("Studies_without_reports.csv", 'w')
+    f.write(nr)
+    f.close()
+
+    #Radiation exposure levels by study year
+    print("Relative Radiation exposure levels")
+
+
+    #Radiation exposure levels by type of Rx (Lateral vs AP)
 
     return summary
 
-def getAllInfoPatient(patient_ID_XNAT):
-    all_studies_DF = pd.read_csv(root + '/Rx-thorax-automatic-captioning/all_info_studies.csv' , sep = ';' , header = 0)
-    rows = all_studies_DF.loc[all_studies_DF['StudyID'] == patient_ID_XNAT]
-    patientID = rows.iloc[0]['PatientID']
-    studies = all_studies_DF.loc[all_studies_DF['PatientID'] == patientID]
+def getAllInfoPatient(patient_ID_XNAT= False , patient_ID=False):
+    pd.set_option('display.max_colwidth', -1)
+    all_studies_DF = pd.read_csv(root + '/Rx-thorax-automatic-captioning' + all_info_studies_file , sep = ';' , header = 0)
+    rows = None
+    if patient_ID_XNAT:
+        rows = all_studies_DF.loc[all_studies_DF['StudyID'] == patient_ID_XNAT]
+        patient_ID = rows.iloc[0]['PatientID']
+    studies = all_studies_DF.loc[all_studies_DF['PatientID'] == patient_ID]
+    #studies.loc[:,'StudyDate'] =studies.loc[:,'StudyDate'].apply(pd.to_numeric) //FIX ordering not working
+    #studies.sort_values(by='StudyDate', ascending=True)
     return studies
 
 if ID_XNAT is not None:
@@ -242,10 +406,19 @@ if modality is not None:
 
 
 if patient_ID_XNAT is not None:
-    print(getAllInfoPatient(patient_ID_XNAT))
+    print(getAllInfoPatient(patient_ID_XNAT =patient_ID_XNAT ))
+
+if patient_ID is not None:
+    print(getAllInfoPatient(patient_ID = patient_ID))
 
 if filename is not None:
     saveAllStudyInfoFullDataset(filename_to_describe)
 
 if filename_to_describe is not None:
     summarizeAllStudies()
+
+if categorical_field is not None:
+    summarizeAllStudiesByCategory(file = all_info_studies_file, category = categorical_field)
+
+if split_images_side_front is not None:
+    splitImagesFrontalSide()
